c5754fff1ddf356ab17889b03779c6aff6b7dd70
Chris McGinn <chris.s.mcginn@live.com>
2013-11-24 07:55:57 -0500

    Working prototype, playing static MP4 stream

--- Television/src/com/greenflag/television/DetailActivity.java
+++ Television/src/com/greenflag/television/DetailActivity.java
@@ -1,10 +1,13 @@
 package com.greenflag.television;
 
+import java.net.URL;
+
 import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
+import android.widget.VideoView;
 
 public class DetailActivity extends Activity {
 
@@ -13,7 +16,14 @@ public class DetailActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_detail);
 		Intent intent = getIntent();
-		
+		VideoView v = (VideoView)findViewById(R.id.videoView);
+		Uri source = Uri.parse("http://pseudo01.hddn.com/vod/demo.flowplayervod/bbb-800.mp4");
+		v.setVideoURI(source);
+		v.start();
+/*		URL source = new URL("http://www.youtube.com/embed/lt416kX0tMY");
+		*/
+		/*MediaPlayer mediaPlayer = new MediaPlayer();
+		mediaPlayer.set*/
 		//String videoUrl = intent.
 		//Log.i("Television",videoUrl);
 	}
